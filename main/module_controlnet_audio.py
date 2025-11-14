f949101226788b6dfcdd99280634b0be4c208c25
chuyiting <eddy.chi.chu@gmail.com>
2025-11-13 19:56:07 -0800

    fix typo

--- main/module_controlnet_audio.py
+++ main/module_controlnet_audio.py
@@ -98,7 +98,7 @@ class Model(pl.LightningModule):
                                                           "seconds_total": total_seconds[i],
                                                           "audio": y[i:i+1]} for i in range(y.shape[0])], 
                                                           device=self.device)
-        print(f"x shape: {noised_input.shape}")
+        print(f"x shape: {noised_inputs.shape}")
         print(f"audio shape {cond['audio'][0].shape}")
 
         output = self.model(x=noised_inputs,
