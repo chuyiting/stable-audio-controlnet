c78ab1bf9db2c66e59041447297cddbf2b2bbe99
chuyiting <eddy.chi.chu@gmail.com>
2025-11-21 13:18:56 -0800

    fix invalid argument

--- main/controlnet/diffusion.py
+++ main/controlnet/diffusion.py
@@ -547,6 +547,22 @@ def create_diffusion_cond_from_config(config: tp.Dict[str, tp.Any]):
         wrapper_fn = ConditionedFiLMDiffusionModelWrapper
         assert diffusion_model_type == 'dit_film' 
 
+    if model_type == "diffusion_cond_film":
+        return wrapper_fn(
+            diffusion_model,
+            conditioner,
+            min_input_length=min_input_length,
+            sample_rate=sample_rate,
+            cross_attn_cond_ids=cross_attention_ids,
+            global_cond_ids=global_cond_ids,
+            input_concat_ids=input_concat_ids,
+            prepend_cond_ids=prepend_cond_ids,
+            film_cond_ids=film_cond_ids,
+            pretransform=pretransform,
+            io_channels=io_channels,
+            **extra_kwargs
+        )
+        
     return wrapper_fn(
         diffusion_model,
         conditioner,
