1e2f8625495b0bc5c61c05107d920e2ae6a46fea
chuyiting <eddy.chi.chu@gmail.com>
2025-11-20 21:33:50 -0800

    remove prints

--- main/module_controlnet_eeg.py
+++ main/module_controlnet_eeg.py
@@ -113,8 +113,6 @@ class Model(pl.LightningModule):
 
         # encode to diffusion latent
         diffusion_input = self.model.pretransform.encode(x_audio)  # shape (B, ...)
-        print(f"x audio: {x_audio.shape}")
-        print(f"diffusion input: {diffusion_input.shape}")
 
         # timesteps
         t = self._sample_timesteps(diffusion_input.shape[0], device)
@@ -150,7 +148,6 @@ class Model(pl.LightningModule):
         cond = self.model.conditioner(cond_items, device=device)
 
         # forward
-        print(f"module controlnet eeg: {noised_inputs.shape}")
         output = self.model(
             x=noised_inputs,
             t=t,
