2252614ea58a8fb219f2a8874172c60ddc47f1b9
chuyiting <eddy.chi.chu@gmail.com>
2025-11-21 02:56:42 -0800

    remove unused condition

--- main/controlnet/pretrained.py
+++ main/controlnet/pretrained.py
@@ -105,9 +105,8 @@ def get_pretrained_film_model(name: str,
     model = create_model_from_config(model_config)
 
     # EEG conditioner does not exist, we need to manually create and add to it
-    if 'eeg' in controlnet_types:
-        eeg_conditioner = EEGConditioner(model_config["model"]["diffusion"]["config"]["global_cond_dim"], eeg_ckpt_path, n_channels = eeg_ch, duration_s=-1)
-        model.conditioner.conditioners['eeg'] = eeg_conditioner
+    eeg_conditioner = EEGConditioner(model_config["model"]["diffusion"]["config"]["global_cond_dim"], eeg_ckpt_path, n_channels = eeg_ch, duration_s=-1)
+    model.conditioner.conditioners['eeg'] = eeg_conditioner
 
 
     # Try to download the model.safetensors file first, if it doesn't exist, download the model.ckpt file
