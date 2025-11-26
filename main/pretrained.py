4a0e877a7b8f12844d3c104a4ee5f97db5ba0aed
chuyiting <eddy.chi.chu@gmail.com>
2025-11-25 17:07:17 -0800

    fix typo

---
+++ main/pretrained.py
@@ -0,0 +1,164 @@
+import json
+from typing import List
+
+import torch
+from stable_audio_tools.inference.generation import generate_diffusion_cond
+from stable_audio_tools.inference.sampling import get_alphas_sigmas
+from stable_audio_tools.models.utils import load_ckpt_state_dict
+
+from main.controlnet.factory import create_model_from_config
+from main.controlnet.conditioner import EEGCondtioner
+
+from huggingface_hub import hf_hub_download
+
+
+
+def get_pretrained_controlnet_model(name: str,
+                                    controlnet_types : List[str],
+                                    depth_factor=0.5,
+                                    eeg_conditioner = None):
+    model_config_path = hf_hub_download(name, filename="model_config.json", repo_type='model')
+
+    with open(model_config_path) as f:
+        model_config = json.load(f)
+    model_config["model_type"] = "diffusion_cond_controlnet"
+    model_config["model"]["diffusion"]['config']["controlnet_depth_factor"] = depth_factor
+    model_config["model"]["diffusion"]["type"] = "dit_controlnet"
+    model_config["model"]["pretransform"]["chunk"] = True
+
+    model_config["model"]["diffusion"]['controlnet_cond_ids'] = []
+    for controlnet_type in controlnet_types:
+        if controlnet_type in ["audio", "envelope", "chroma"]:
+            controlnet_conditioner_config = {"id": controlnet_type,
+                                             "type": "pretransform",
+                                             "config": {"sample_rate": model_config["sample_rate"],
+                                                        "output_dim": model_config["model"]["pretransform"]["config"]["latent_dim"],
+                                                       "pretransform_config": model_config["model"]["pretransform"]}}
+            model_config["model"]['conditioning']['configs'].append(controlnet_conditioner_config)
+            model_config["model"]["diffusion"]['controlnet_cond_ids'].append(controlnet_type)
+        
+        if controlnet_type == 'eeg':
+            # EEG conditioner does not exist, we need to manually create and add to it
+            controlnet_conditioner_config = {"id": controlnet_type,
+                                             "type": "number", # just placeholder, will replace later
+                                             "config": {}} # dont care
+            model_config["model"]['conditioning']['configs'].append(controlnet_conditioner_config)
+            model_config["model"]["diffusion"]['controlnet_cond_ids'].append(controlnet_type)
+            # TODO global conditioning and cross attention conditioning
+            # model_config['model']['diffusion']['cross_attention_cond_ids'].append(controlnet_type)
+            # model_config['model']['diffusion']['global_cond_ids'].append(controlnet_type)
+
+    model = create_model_from_config(model_config)
+
+    # EEG conditioner does not exist, we need to manually create and add to it
+    if 'eeg' in controlnet_types:
+        assert eeg_conditioner is not None, "eeg_conditioner must be provided if 'eeg' is in controlnet_types"
+        assert eeg_conditioner.use_film == False, "eeg_conditioner.use_film must be False for controlnet model"
+        model.conditioner.conditioners['eeg'] = eeg_conditioner
+
+    # Try to download the model.safetensors file first, if it doesn't exist, download the model.ckpt file
+    try:
+        model_ckpt_path = hf_hub_download(name, filename="model.safetensors", repo_type='model')
+    except Exception as e:
+        model_ckpt_path = hf_hub_download(name, filename="model.ckpt", repo_type='model')
+
+    state_dict = load_ckpt_state_dict(model_ckpt_path)
+
+    model.load_state_dict(state_dict, strict=False)
+    state_dict_controlnet = {k.split('model.model.')[-1]: v for k, v in state_dict.items() if k.startswith('model.model')}
+    model.model.controlnet.load_state_dict(state_dict_controlnet, strict=False)
+
+    for controlnet_type in controlnet_types:
+        if controlnet_type in ["audio", "envelope", "chroma"]:
+            state_dict_pretransform = {k: v for k, v in state_dict.items() if k.startswith('pretransform.')}
+            model.conditioner.conditioners[controlnet_type].load_state_dict(state_dict_pretransform)
+
+    return model, model_config
+
+def get_pretrained_film_model(name: str, eeg_conditioner = None):
+    model_config_path = hf_hub_download(name, filename="model_config.json", repo_type='model')
+
+    with open(model_config_path) as f:
+        model_config = json.load(f)
+    model_config["model_type"] = "diffusion_cond_film"
+    model_config["model"]["diffusion"]["type"] = "dit_film"
+    model_config["model"]["pretransform"]["chunk"] = True
+
+    # TODO remove
+    model_config["model"]["diffusion"]['controlnet_cond_ids'] = []
+    model_config["model"]["diffusion"]['film_cond_ids'] = []
+
+    # EEG conditioner does not exist, we need to manually create and add to it
+    eeg_config = {"id": "eeg",
+                    "type": "number", # just placeholder, will replace later
+                    "config": {}} # dont care
+
+    model_config["model"]['conditioning']['configs'].append(eeg_config)
+    model_config["model"]["diffusion"]['film_cond_ids'].append("eeg")
+    # TODO global conditioning and cross attention conditioning
+    # model_config['model']['diffusion']['cross_attention_cond_ids'].append(controlnet_type)
+    # model_config['model']['diffusion']['global_cond_ids'].append(controlnet_type)
+
+    model = create_model_from_config(model_config)
+
+    # EEG conditioner does not exist, we need to manually create and add to it
+    assert eeg_conditioner is not None, "eeg_conditioner must be provided for film model"
+    assert eeg_conditioner.use_film == True, "eeg_conditioner.use_film must be True for film model"
+    model.conditioner.conditioners['eeg'] = eeg_conditioner
+
+
+    # Try to download the model.safetensors file first, if it doesn't exist, download the model.ckpt file
+    try:
+        model_ckpt_path = hf_hub_download(name, filename="model.safetensors", repo_type='model')
+    except Exception as e:
+        model_ckpt_path = hf_hub_download(name, filename="model.ckpt", repo_type='model')
+
+    state_dict = load_ckpt_state_dict(model_ckpt_path)
+    model.load_state_dict(state_dict, strict=False)
+
+    return model, model_config
+
+
+if __name__ == '__main__':
+    from torch.utils.data import DataLoader
+    from main.data.dataset_musdb import create_musdb_dataset, collate_fn_conditional
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
+    model, model_config = get_pretrained_controlnet_model("stabilityai/stable-audio-open-1.0")
+    model = model.cuda()
+
+    sample_size = model_config["sample_size"]
+    sample_rate = model_config["sample_rate"]
+
+    dataset = create_musdb_dataset("../../data/musdb18hq/train.tar",
+                                      sample_rate=44100,
+                                      chunk_dur=47.57)
+    dataloader = DataLoader(dataset,
+                            batch_size=1,
+                            pin_memory=False,
+                            drop_last=True,
+                            collate_fn=collate_fn_conditional,
+                            num_workers=0)
+    data_x, data_y, data_z = next(iter(dataloader))
+
+    conditioning = [{
+        "audio": data_y.unsqueeze(1).repeat_interleave(2, dim=1).cuda(),
+        "prompt": data_z[0],
+        "seconds_start": 0,
+        "seconds_total": 40
+    }]
+
+    # Generate stereo audio
+    output = generate_diffusion_cond(
+         model,
+         steps=100,
+         cfg_scale=7,
+         conditioning=conditioning,
+         sample_size=sample_size,
+         sigma_min=0.3,
+         sigma_max=500,
+         sampler_type="dpmpp-3m-sde",
+         device=device
+    )
+
